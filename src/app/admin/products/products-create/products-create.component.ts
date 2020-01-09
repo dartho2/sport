92c3458aacfb17d47ba833ad28306a8bfd1181ca
Jan Nowak <jannowak@example.com>
2020-01-09 23:18:14 +0100

    re

--- src/app/admin/products/products-create/products-create.component.ts
+++ src/app/admin/products/products-create/products-create.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, FormArray, FormControl, Validators } from '@angular/forms';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
-import { Product} from '../product.model'
+import { Product } from '../product.model'
 import { ProductService } from '../product.service';
 
 @Component({
@@ -15,8 +15,8 @@ export class ProductsCreateComponent implements OnInit {
   selectedOptions;
   selectPackage;
   productId;
-  value: ['kg', 'szt' , 'l'];
-  valueSupplier: ['KŚ', 'WoA' , 'W'];
+  value: ['kg', 'szt', 'l'];
+  valueSupplier: ['KŚ', 'WoA', 'W'];
   mode;
   bodyForm: FormGroup;
   selectedValue;
@@ -25,30 +25,26 @@ export class ProductsCreateComponent implements OnInit {
   message;
 
   constructor(private _fb: FormBuilder, private route: ActivatedRoute, private router: Router, private productService: ProductService) {
-    
+
   }
 
   ngOnInit() {
-    console.log(this.pr_data)
     this.bodyForm = new FormGroup({
       _id: new FormControl(null),
       name: new FormControl(null),
       description: new FormControl(null),
-      label: new FormControl(null),
-      logo: new FormControl(null),
       image: new FormControl(null),
-      style: new FormControl(null),
-      active: new FormControl(null),
-      price: new FormControl(null),
+      nettoPrice: new FormControl(null),
       unit: new FormControl(null),
+      qty: new FormControl(null),
       weight: new FormControl(null),
       vat: new FormControl(null),
-      totalPrice: new FormControl(null),
-      product_data: new FormControl(null),
+      bruttoPrice: new FormControl(null),
+      productDate: new FormControl(null),
       supplier: new FormControl(null),
       history: this._fb.array([])
     });
-    
+
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if (paramMap.has("idProduct")) {
         this.mode = "edit";
@@ -66,84 +62,68 @@ export class ProductsCreateComponent implements OnInit {
       _id: [data ? data._id : null],
       name: [data ? data.name : '',],
       description: [data ? data.description : '',],
-      label: [data ? data.label : '',],
-      logo: [data ? data.logo : '',],
       image: [data ? data.image : '',],
-      style: [data ? data.style : '',],
-      active: [data ? data.active : '',],
-      price:[data ? data.price : '',],
+      nettoPrice: [data ? data.nettoPrice : '',],
       unit: [data ? data.unit : '',],
+      qty: [data ? data.qty : '',],
       weight: [data ? data.weight : '',],
       vat: [data ? data.vat : '',],
-      totalPrice: [data ? data.totalPrice : '',],
+      bruttoPrice: [data ? data.bruttoPrice : '',],
       supplier: [data ? data.supplier : '',],
-      product_data: [data ? data.product_data : '',],
+      productDate: [data ? data.productDate : '',],
       history: this._fb.array(
         this.getHistory(data ? data.history : null)
       )
-      // content: this._fb.array(
-      //   this.getContent(data ? data.content : null),
-      // ),
-      // tags: this._fb.array(
-      //   this.getTags(data ? data.tags : null)
-      // ),
     })
   }
   getHistory(history: any): FormGroup[] {
     return history ? history.map(historyBody => {
       return this._fb.group({
         supplier: [historyBody.supplier],
-        price: [historyBody.price],
-        totalPrice: [historyBody.totalPrice],
+        nettoPrice: [historyBody.nettoPrice],
+        bruttoPrice: [historyBody.bruttoPrice],
+        qty: [historyBody.qty],
         vat: [historyBody.vat],
-        product_data: [historyBody.product_data],
+        productDate: [historyBody.productDate],
       });
     }) : []
   }
   onAddContent() {
     if (this.mode === "edit") {
-      this.bodyForm.controls['totalPrice'].patchValue(this.calculateTotalPrice(this.bodyForm.value)) 
-      this.bodyForm.controls['product_data'].patchValue(this.pr_data);
-      // (<FormArray>this.bodyForm.controls['history']).value.push({
-      //   "supplier": this.bodyForm.value.supplier,
-      //   "price": this.bodyForm.value.price,
-      //   "totalPrice": this.bodyForm.value.totalPrice,
-      //   "vat": this.bodyForm.value.vat,
-      //   "product_data": this.bodyForm.value.product_data
-      // });
+      this.bodyForm.controls['bruttoPrice'].patchValue(this.calculateTotalPrice(this.bodyForm.value))
+      this.bodyForm.controls['productDate'].patchValue(this.pr_data);
       this.productService.updateProduct(this.bodyForm.value).subscribe(response => {
         this.message = response;
       })
     } else {
       delete this.bodyForm.value._id
-      this.bodyForm.value.totalPrice = this.calculateTotalPrice(this.bodyForm.value)
-      this.productService.createProduct(this.bodyForm.value).subscribe(()=>
-      {
-        this.router.navigate(["../"], {relativeTo: this.route});
+      this.bodyForm.value.bruttoPrice = this.calculateTotalPrice(this.bodyForm.value)
+      this.productService.createProduct(this.bodyForm.value).subscribe(() => {
+        this.router.navigate(["../"], { relativeTo: this.route });
       })
     };
   }
-  calculateTotalPrice(price){
+  calculateTotalPrice(price) {
     this.bodyForm.value.price = this.bodyForm.value.price.replace(',', '.');
     this.bodyForm.value.weight = this.bodyForm.value.weight.replace(',', '.');
-   
-    this.bodyForm.controls['product_data'].patchValue(this.pr_data);
-If (price.price === 0) {
-this.bodyForm.value.totalPrice = (price.vat).toFixed(2);
-} else {
-this.bodyForm.value.totalPrice = (price.price * (price.vat / 100 + 1)).toFixed(2);
-}  
-    this.bodyForm.controls['totalPrice'].patchValue(this.bodyForm.value.totalPrice);
-     (<FormArray>this.bodyForm.controls['history']).value.push({
+    this.bodyForm.controls['productDate'].patchValue(this.pr_data);
+    if (price.price === 0) {
+      this.bodyForm.value.bruttoPrice = (price.vat).toFixed(2);
+    } else {
+      this.bodyForm.value.bruttoPrice = (price.price * (price.vat / 100 + 1)).toFixed(2);
+    }
+    this.bodyForm.controls['bruttoPrice'].patchValue(this.bodyForm.value.bruttoPrice);
+    (<FormArray>this.bodyForm.controls['history']).value.push({
       "supplier": this.bodyForm.value.supplier,
-      "price": this.bodyForm.value.price,
-      "totalPrice": this.bodyForm.value.totalPrice,
+      "nettoPrice": this.bodyForm.value.nettoPrice,
+      "bruttoPrice": this.bodyForm.value.bruttoPrice,
+      "qty": this.bodyForm.value.qty,
       "vat": this.bodyForm.value.vat,
-      "product_data": this.bodyForm.value.product_data
+      "productDate": this.bodyForm.value.productDate
     });
-    return  this.bodyForm.value.totalPrice
+    return this.bodyForm.value.bruttoPrice
   }
-  gotoBack(){
-    this.router.navigate(["../"], {relativeTo: this.route});
+  gotoBack() {
+    this.router.navigate(["../"], { relativeTo: this.route });
   }
 }
--- src/app/admin/products/products-list/products-list.component.ts
+++ src/app/admin/products/products-list/products-list.component.ts
@@ -1,10 +1,7 @@
-import { Component, OnInit, OnDestroy, ViewChild, AfterViewInit } from '@angular/core';
-import {MatTableDataSource} from '@angular/material';
-import {MatPaginator, MatSort} from '@angular/material';
-import { ActivatedRoute, ParamMap } from '@angular/router';
-import { switchMap } from 'rxjs/operators';
-import { Observable, Subscriber, Subscription } from 'rxjs';
-import { Product } from '../product.model';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
+import { MatPaginator, MatSort } from '@angular/material';
+import { ActivatedRoute } from '@angular/router';
 import { ProductService } from '../product.service';
 
 
@@ -14,50 +11,38 @@ import { ProductService } from '../product.service';
   styleUrls: ['./products-list.component.css']
 })
 export class ProductsListComponent implements AfterViewInit, OnInit {
-    productData;
-    product;
-    dataSource = new MatTableDataSource(this.productData);
-    displayedColumns: string[] = [
+  productData;
+  product;
+  dataSource = new MatTableDataSource(this.productData);
+  displayedColumns: string[] = [
     'name',
-    "price" ,
+    "nettoPrice",
     "unit",
     "weight",
     "vat",
     "_id",
-  "totalPrice" ];
+    "bruttoPrice"];
 
-    @ViewChild(MatPaginator) paginator: MatPaginator;
-    @ViewChild(MatSort) sort: MatSort;
-    applyFilter(filterValue: string) {
-      this.dataSource.filter = filterValue.trim().toLowerCase();
-    }
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 
-  constructor(private route: ActivatedRoute, private productService: ProductService) { 
+  constructor(private route: ActivatedRoute, private productService: ProductService) {
     this.productService.getProduct().subscribe(response => {
       this.product = response
-     this.productData = this.product;
-     this.dataSource.data = this.productData;  
-     console.log(this.product)
+      this.productData = this.product;
+      this.dataSource.data = this.productData;
     });
-    }
-    ngAfterViewInit() {
-      this.dataSource.paginator = this.paginator;
-      this.dataSource.sort = this.sort;
-     }
-    getmember(a) {
-      return parseFloat(a).toFixed(2)
-    }  
-    // calculate(a) {
-    //   return parseFloat(a)*5;
-    // }
-  ngOnInit() {
-  
-    // this.route.paramMap.subscribe(param => {
-    //   this.productService.getProduct().subscribe(response => {
-    //     this.product = response
-    //   })
-    // })
-
   }
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+  getmember(a) {
+    return parseFloat(a).toFixed(2)
+  }
+  ngOnInit() { }
 
 }
