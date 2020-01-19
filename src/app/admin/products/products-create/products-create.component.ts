0574583a6b9cb7bdb72802fe5c8ca796484ddf70
Jan Nowak <jannowak@example.com>
2020-01-19 01:50:29 +0100

    recipe f1

--- src/app/admin/products/products-create/products-create.component.ts
+++ src/app/admin/products/products-create/products-create.component.ts
@@ -3,7 +3,11 @@ import { FormGroup, FormBuilder, FormArray, FormControl, Validators } from '@ang
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { Product } from '../product.model'
 import { ProductService } from '../product.service';
-
+import { Observable } from 'rxjs';
+import { startWith, map } from 'rxjs/operators';
+  export interface Product {
+    name: string;
+  }
 @Component({
   selector: 'app-products-create',
   templateUrl: './products-create.component.html',
@@ -15,15 +19,34 @@ export class ProductsCreateComponent implements OnInit {
   foodCost;
   @Input() 
   ProductDish;
-
+  productSelected;
+  filteredOptions: Observable<Product[]>;
+  myControl = new FormControl();
   date = new FormControl(new Date());
   serializedDate = new FormControl((new Date()).toISOString());
   selectedOptions;
   selectPackage;
   productId;
   value: ['kg', 'szt', 'l'];
-  valueSupplier: ['KŚ', 'WoA', 'W', 'Pp', 'In'];
+  valueSupplier: ['KŚ', 'WoA', 'W', 'Pp', 'Sk', 'In'];
   mode;
+  options: Product[] = [];
+  productRecipe;
+  data = {
+    recipe: [
+      {
+        id: '',
+        name: '',
+        nettoPrice: '',
+        bruttoPrice: '',
+        weight: '',
+        vat: '',
+        unit: '',
+        productWeight: '',
+        valueProduct: ''
+      }
+    ]
+  }
   bodyForm: FormGroup;
   selectedValue;
   product: Product;
@@ -31,10 +54,10 @@ export class ProductsCreateComponent implements OnInit {
   message;
 
   constructor(private _fb: FormBuilder, private route: ActivatedRoute, private router: Router, private productService: ProductService) {
-
-  }
-
-  ngOnInit() {
+    this.productService.getProduct().subscribe(response => {
+      this.productRecipe = response
+      this.options =this.productRecipe
+    });
     if(this.foodCost){
       this.buildFormforProducts(this.foodCost)
     }else{
@@ -53,7 +76,8 @@ export class ProductsCreateComponent implements OnInit {
       supplier: new FormControl(null),
       losses: new FormControl(null),
       lossesPriceNetto: new FormControl(null),
-      history: this._fb.array([])
+      history: this._fb.array([]),
+      recipe: this._fb.array([])
     });
   }
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
@@ -67,6 +91,62 @@ export class ProductsCreateComponent implements OnInit {
         this.mode = "create";
       }
     });
+
+    this.setCities();
+  }
+  get formData() {
+    return <FormArray>this.bodyForm.get('recipe');
+  }
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges
+    .pipe(
+      startWith(''),
+      map(value => typeof value === 'string' ? value : value.name),
+      map(name => name ? this._filter(name) : this.options.slice())
+    );
+  }
+  displayFn(product?: Product): String | undefined {
+    return product ? product.name : undefined;
+  }
+
+  private _filter(name: string): Product[] {
+    const filterValue = name.toLowerCase();
+    return this.options.filter(option => option.name.toLowerCase().includes(filterValue));
+  }
+
+  onChange(selectedValue, y) {
+    this.productSelected = this.productRecipe.filter(item => item.name === selectedValue.name);
+    let control = (<FormArray>this.bodyForm.controls.recipe).at(y);
+    this.productSelected.forEach(x => {
+         control.setValue({
+           id: x._id,
+          name: x.name, 
+          nettoPrice: x.nettoPrice, 
+          bruttoPrice: x.bruttoPrice, 
+          weight: x.weight,
+          unit: x.unit,
+          vat: x.vat,
+          productWeight: x.productWeight? x.productWeight : ''  ,
+          valueProduct: x.valueProduct? x.valueProduct  :''
+        })
+    });
+  }
+ 
+  setCities(){
+    let control = <FormArray>this.bodyForm.controls.recipe;
+    this.data.recipe.forEach(x => {
+      control.push(this._fb.group({  
+        id: x.id,
+        name: x.name, 
+        nettoPrice: x.nettoPrice, 
+        bruttoPrice: x.bruttoPrice, 
+        weight: x.weight,
+        vat: x.vat,
+        unit: x.unit,
+        productWeight: x.productWeight,
+        valueProduct: x.valueProduct,
+        }))
+    })
   }
   buildFormforProducts(foodCost): FormGroup {
     return this.bodyForm = this._fb.group({
@@ -86,6 +166,9 @@ export class ProductsCreateComponent implements OnInit {
       productDate: '',
       history: this._fb.array(
         this.getHistory(null)
+      ),
+      recipe: this._fb.array(
+        this.getRecipe(null)
       )
     })
   }
@@ -108,8 +191,27 @@ export class ProductsCreateComponent implements OnInit {
       productDate: [data ? data.productDate : '',],
       history: this._fb.array(
         this.getHistory(data ? data.history : null)
+      ),
+      recipe: this._fb.array(
+        this.getRecipe(data ? data.recipe : null)
       )
     })
+  }
+  getRecipe(recipe: any): FormGroup[] {
+    return recipe ? recipe.map(recipeBody => {
+      return this._fb.group({
+        name: [recipeBody.name],
+        nettoPrice: [recipeBody.nettoPrice],
+        bruttoPrice: [recipeBody.bruttoPrice],
+        weight: [recipeBody.weight],
+        unit: [recipeBody.unit],
+        vat: [recipeBody.vat],
+        productDate: [recipeBody.productDate],
+        id: [recipeBody.id],
+        productWeight: [recipeBody.productWeight],
+        valueProduct: [recipeBody.valueProduct]
+      });
+    }) : []
   }
   getHistory(history: any): FormGroup[] {
     return history ? history.map(historyBody => {
@@ -169,5 +271,21 @@ export class ProductsCreateComponent implements OnInit {
   }
   gotoBack() {
     this.router.navigate(["../"], { relativeTo: this.route });
+  }
+  addNewCity() {
+    let control = <FormArray>this.bodyForm.controls.recipe;
+    control.push(
+      this._fb.group({
+        id: '',
+        name: '',
+        nettoPrice: '',
+        bruttoPrice: '',
+        weight: '',
+        vat: '',
+        unit: '',
+        productWeight: '',
+        valueProduct: ''
+      })
+    )
   }
 }
