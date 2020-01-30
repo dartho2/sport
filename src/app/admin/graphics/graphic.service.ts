9f22273418ac69ce71285df211dbb4de65d21c72
dartho2 <wegrzyn_pawel@outlook.com>
2020-01-30 16:36:50 +0100

    graphic

---
+++ src/app/admin/graphics/graphic.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Subject, Observable } from "rxjs";
+import { Router } from "@angular/router";
+import { Graphic } from "./graphic.model";
+
+
+@Injectable({ providedIn: 'root' })
+export class GraphicService {
+    private products: Graphic[] = [];
+    private productsUpdated = new Subject<Graphic[]>();
+    constructor(private _http: HttpClient, private router: Router) { }
+
+    getGraphic(): Observable<Graphic[]> {
+        return
+        [{'date': '1.2020','items':[["dupa1",true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],["dupa2",false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true],["dupa3",false,true,false,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],["dupa4",false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,false,false,false]]},
+        {'date': '12.2019','items':[["dupa12",true,false,true,false,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],["dupa2",false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true],["dupa3",false,true,false,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],["dupa4",false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,false,false,false]]}]
+    }
+    //   createProduct(bodyProduct) {
+    //     return this._http.post<Product[]>("https://karmazdrowia.pl:8080/api/products", bodyProduct);
+    //   }
+    //   getProductID(id: string) {
+    //     return this._http.get<Product>("https://karmazdrowia.pl:8080/api/products/" + id);
+    //   }
+    //   updateProduct(product) {
+    //     return this._http.post("https://karmazdrowia.pl:8080/api/products/" + product._id, product);
+    //   }
+    //   deleteProduct(id) {
+    //     return this._http.delete<Product[]>("https://karmazdrowia.pl:8080/api/products/" + id)
+    // }
+
+}
--- src/app/admin/graphics/graphics-list/graphics-list.component.ts
+++ src/app/admin/graphics/graphics-list/graphics-list.component.ts
@@ -1,4 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { GraphicService } from '../graphic.service';
+import {FormControl} from '@angular/forms';
+import {MomentDateAdapter, MAT_MOMENT_DATE_ADAPTER_OPTIONS} from '@angular/material-moment-adapter';
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
+import {MatDatepicker} from '@angular/material/datepicker';
+import * as _moment from 'moment';
+import {default as _rollupMoment, Moment} from 'moment';
+
+const moment = _rollupMoment || _moment;
+
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'MM/YYYY',
+  },
+  display: {
+    dateInput: 'MM/YYYY',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
+
 export interface PeriodicElement {
 
 }
@@ -7,64 +29,91 @@ const ELEMENT_DATA: PeriodicElement[] = [];
 @Component({
   selector: 'app-graphics-list',
   templateUrl: './graphics-list.component.html',
-  styleUrls: ['./graphics-list.component.css']
+  styleUrls: ['./graphics-list.component.css'],
+  providers: [
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS]
+    },
+
+    {provide: MAT_DATE_FORMATS, useValue: MY_FORMATS},
+  ],
 })
 export class GraphicsListComponent implements OnInit {
+  date = new FormControl(moment());
+
+  chosenYearHandler(normalizedYear: Moment) {
+    const ctrlValue = this.date.value;
+    ctrlValue.year(normalizedYear.year());
+    this.date.setValue(ctrlValue);
+  }
+
+  chosenMonthHandler(normalizedMonth: Moment, datepicker: MatDatepicker<Moment>) {
+    const ctrlValue = this.date.value;
+    ctrlValue.month(normalizedMonth.month());
+    this.date.setValue(ctrlValue);
+    datepicker.close();
+  }
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
   dataSource = ELEMENT_DATA;
   numberOfDays;
   week;
-  rowN =[
-    "a","b","c","p"
-  ]
+  // rowN = [
+  //   ["a"], ["b"], ["c"], ["p"]
+  // ]
+  rowN =  [{ "date": "",
+   'items': []
+  }]
+  userG = [["p"],["a"]];
+  rowGraphic;
   datag: any[] = [];
   dataf: any[] = [];
   datafort: any;
-  constructor() { }
+  month: number;
+  year: number;
+  graphic: any[] = [];
+  userGraphic: any[] = [];
+  constructor(private grapicService: GraphicService) { }
+  getColor(dayNumber) {
+    let day = parseInt(dayNumber)
+    if (!isNaN(day)) {
+      let today = new Date(this.month + '/' + day + '/' + this.year).getDay()
+      return today === 0 || today === 6 ? '#b7b7b7' : ''
+    }
 
+  }
   ngOnInit() {
-   
-    console.log(this.dataSource)
-    var y = new Date().getFullYear()
-    var m = (new Date().getMonth()+1)
-    console.log('y/m', y , m)
-  var firstOfMonth = new Date(y, m-1,1)
-  console.log('first', firstOfMonth.getDay())
-  if (firstOfMonth.getDay()< 6)
-  {
-  this.week = 6-firstOfMonth.getDay()
-  console.log('week', this.week)
+    this.grapicService.getGraphic()
+    this.setDateGraphic()
+    this.rowN.map(x=>{
+    x.date.length === 0 ? x.date = this.month+'.'+this.year: x.date ;
+      x['items'].push(...this.userG);
+      x['items'].forEach((a: any) =>{
+          if(Array.isArray(a)){
+          a.push(...this.userGraphic)
+        } 
+      })
+      this.rowGraphic = x['items'];
+    })
+    console.log(this.rowN)
+this.dataSource = this.rowGraphic
+
   }
-    var lastOfMonth = new Date(y,m, 0).getDate()
-    this.numberOfDays = Array(lastOfMonth)
-  //  this.numberOfDays.map(function (x, i) { return 'a' })
-  this.numberOfDays = Array.apply(null, Array(lastOfMonth)).map(function (x, i) { return (i+1).toString(); })
-  // this.numberOfDays.push("pawel").at(0)
-  console.log('aarray', this.numberOfDays)
-  var dd = this.numberOfDays
-  this.numberOfDays.unshift("Name")
-  console.log(' this.numberOfDays',  this.numberOfDays)
-  //  this.tableHeder(this.numberOfDays)
-  // this.datag = this.numberOfDays
-  this.datafort = Array.apply(null, Array(lastOfMonth)).map(function (x, i) { return false })
-  this.datafort.unshift("Pawel")
-  this.datag.push([this.datafort])
- this.datag.push([this.datafort])
-  console.log('dataf', this.datag)
- this.dataSource = this.datag
-   this.displayedColumns = this.numberOfDays
-    // var used = firstOfMonth.getDay() +6 + lastOfMonth.getDay();
-    // this.numberOfDays = Math.ceil(used / 7) 
-    console.log('this.dataSource', this.dataSource)
-    console.log('displayedColumns',this.displayedColumns)
+  setDateGraphic() {
+    this.year = new Date().getFullYear()
+    this.month = (new Date().getMonth() + 1)
+    let firstOfMonth = new Date(this.year, this.month - 1, 1)
+    let lastOfMonth = new Date(this.year, this.month, 0).getDate()
+    this.numberOfDays = Array.apply(null, Array(lastOfMonth)).map(function (x, i) { return (i + 1).toString(); })
+    this.userGraphic = Array.apply(null, Array(lastOfMonth)).map(function (x, i) { return false })
+    this.numberOfDays.unshift("Name")
+    this.displayedColumns = this.numberOfDays
+
   }
-  // tableHeder(length){
-  //   for(var i = 0; i < length; i++) {
-  //     data.push(createSomeObject());
-  // }
-  
-  isSelectrd(x){
+
+  isSelectrd(x) {
     return Boolean(JSON.parse(x))
   }
-  
+
 }
